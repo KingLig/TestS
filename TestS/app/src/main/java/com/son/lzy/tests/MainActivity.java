8a7b64535abe3f489158947ac6b054a2905bf05f
King <624771244@qq.com>
2017-08-23 14:47:56 +0800

    增加号码

--- TestS/app/src/main/java/com/son/lzy/tests/MainActivity.java
+++ TestS/app/src/main/java/com/son/lzy/tests/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String name;
+    private String Number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
