9d312067cd5627df672eb6c941c2b888904e7d9b
King <624771244@qq.com>
2017-08-23 14:46:19 +0800

    增加姓名

---
+++ TestS/app/src/main/java/com/son/lzy/tests/MainActivity.java
@@ -0,0 +1,14 @@
+package com.son.lzy.tests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
